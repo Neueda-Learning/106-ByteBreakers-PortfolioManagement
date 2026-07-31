4e5de9516b6149c72501354780129a629d2ba8e0
Divyansh <divyanshpankholi78@gmail.com>
2026-07-31 11:30:55 +0000

    Adds schema files for the db

---
+++ Backend/src/main/resources/schema.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS user_investments;
+DROP TABLE IF EXISTS investment_options;
+
+
+CREATE TABLE investment_options (
+
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL,
+
+    category VARCHAR(50) NOT NULL,
+
+    current_price DECIMAL(12,2) NOT NULL,
+
+    trend VARCHAR(20) NOT NULL,
+
+    estimated_return DECIMAL(5,2)
+
+);
+
+
+CREATE TABLE user_investments (
+
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    investment_option_id BIGINT NOT NULL,
+
+    quantity DECIMAL(12,2) NOT NULL,
+
+    bought_price DECIMAL(12,2) NOT NULL,
+
+    purchase_date DATE NOT NULL,
+
+    CONSTRAINT fk_investment_option
+        FOREIGN KEY (investment_option_id)
+        REFERENCES investment_options(id)
+
+);
