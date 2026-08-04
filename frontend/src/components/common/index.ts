d3eef95aacef1d5cfcfe4a38fffb8e252a9f8a34
Divyansh <divyanshpankholi78@gmail.com>
2026-08-04 21:22:08 +0530

    Adds DataTable and supporting components

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -3,3 +3,6 @@ export { default as StatCard } from "./StatCard";
 export { default as StatusBadge } from "./StatusBadge";
 export { default as SearchBar } from "./SearchBar";
 export { default as EmptySpace } from "./EmptySpace";
+export { default as Toolbar } from "./Toolbar";
+export { default as DataTable } from "./DataTable";
+export { default as DataTablePagination } from "./DataTablePagination";
