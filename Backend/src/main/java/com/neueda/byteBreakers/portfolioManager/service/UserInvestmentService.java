2b3ef132bda3c0c979186a81bb8ea2be2747cced
Chandanatj13 <chandanatj2005@gmail.com>
2026-08-04 09:11:58 +0000

    Added user investment service implementation

--- Backend/src/main/java/com/neueda/byteBreakers/portfolioManager/service/UserInvestmentService.java
+++ Backend/src/main/java/com/neueda/byteBreakers/portfolioManager/service/UserInvestmentService.java
@@ -16,4 +16,20 @@ public class UserInvestmentService
     public List<UserInvestment> getAllUserInvestments() {
         return userInvestmentRepository.getAllUserInvestments();
     }
+
+    public int createUserInvestment(UserInvestment userInvestment) {
+        return userInvestmentRepository.createUserInvestment(userInvestment);
+    }
+
+    public UserInvestment getUserInvestmentById(Long id) {
+        return userInvestmentRepository.getUserInvestmentById(id);
+    }
+
+    public int updateUserInvestment(Long id, UserInvestment userInvestment) {
+        return userInvestmentRepository.updateUserInvestment(id, userInvestment);
+    }
+
+    public int deleteUserInvestment(Long id) {
+        return userInvestmentRepository.deleteUserInvestment(id);
+    }
 }
