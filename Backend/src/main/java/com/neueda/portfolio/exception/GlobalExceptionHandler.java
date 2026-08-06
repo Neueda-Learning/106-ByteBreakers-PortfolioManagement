5e9942b622747cdcf334f52c9241ec69ef224248
Rohitkumar367 <rohitddroid@gmail.com>
2026-08-06 05:08:33 +0000

    add exception handling for current holding repository

---
+++ Backend/src/main/java/com/neueda/portfolio/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.neueda.portfolio.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(HoldingNotFoundException.class)
+    public ResponseEntity<String> handleHoldingNotFoundException(
+            HoldingNotFoundException ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+
+    @ExceptionHandler(InvestmentDetailsNotFoundException.class)
+    public ResponseEntity<String> handleInvestmentDetailsNotFoundException(
+            InvestmentDetailsNotFoundException ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> genericHandleException(Exception ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
